c6287c33ac68e7dac006de6e9ce79a54bcef1813
Subhan-anwer <sheikhanwer30@gmail.com>
2024-12-30 22:08:27 +0500

    Done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
@@ -10,9 +11,30 @@ const page = () => {
             </div>
 
             {/* Content */}
-            <div className="w-[1180px] mx-auto border border-yellow-400 flex items-center justify-between">
-                <div className='w-[575px] h-[385px] bg-red-300'></div>
-                <div className='w-[575px] h-[384px] bg-blue-300'></div>
+            <div className="max-w-[1180px] h-auto mx-auto border border-yellow-400 flex flex-wrap gap-[21px] items-center justify-center mb-[93px]">
+                <div className='lg:w-[575px] w-full min-h-[385px] bg-red-300 flex flex-col gap-[21px] sm:p-0 p-3'>
+                    <div className="flex flex-col sm:gap-4 gap-2">
+                        <label htmlFor="name" className='text-[#000000] sm:text-xl text-base'>Name</label>
+                        <input id="name" type="text" className='w-full h-[38px] border border-black bg-[#EEEEEE] rounded-md pl-2' />
+                    </div>
+
+                    <div className="flex flex-col sm:gap-4 gap-2">
+                        <label htmlFor="name" className='text-[#000000] sm:text-xl text-base'>Name</label>
+                        <input id="name" type="text" className='w-full h-[38px] border border-black bg-[#EEEEEE] rounded-md pl-2' />
+                    </div>
+
+                    <div className="flex flex-col sm:gap-4 gap-2">
+                        <label htmlFor="msg" className='text-[#000000] sm:text-xl text-base'>Message</label>
+                        <textarea name="msg" id='msg' className='w-full min-h-[75px] border border-black bg-[#EEEEEE] rounded-md pl-2'></textarea>
+                    </div>
+
+                    <button className='w-full h-[48px] sm:text-[20px] text-base text-center rounded-md bg-[#212529] transition-all focus:outline-none hover:bg-[#2b3035] active:bg-[#141618] text-white'>Submit</button>
+                </div>
+
+
+                <div className='lg:w-[575px] w-full lg:p-0 p-2'>
+                    <Image className='w-full h-auto' src='/map.png' alt='Map Image' width={575} height={384} quality={100} />
+                </div>
             </div>
         </div>
     )
